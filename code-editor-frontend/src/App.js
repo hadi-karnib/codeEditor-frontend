c758d14be5509fab8871b3527d068140c5c0aadd
karniib <hadikarnib03@gmail.com>
2024-07-26 23:28:58 +0300

    Added new dependencies to package.json, updated package-lock.json, and modified App.js to include new imports and routes.

--- code-editor-frontend/src/App.js
+++ code-editor-frontend/src/App.js
@@ -1,29 +1,19 @@
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import Login from "./pages/Login/Login";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  Outlet,
 } from "react-router-dom";
-import { RouterProvider} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      {
       <Route path="/" element={<LandingPage />} />
-
-      /*
-        <Route path="/" element={<Home />} />
-        <Route path="/Services" element={<ServicesSection />} />
-        <Route path="/services/:serviceId" element={<ServiceDetail />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/contact" element={<ContactUsSection />} />
-        <Route path="/Login" exact element={<Login />} />
-      </Route> 
-      */}
+      <Route path="/login" element={<Login />} />
     </>
   )
 );
