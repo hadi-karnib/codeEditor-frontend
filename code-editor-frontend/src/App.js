83b002b18686fa508e0d3a05e7da2285c92f10ff
husseinassidi <husseinassidi@gmail.com>
2024-07-26 22:02:22 +0300

    navbar added

--- code-editor-frontend/src/App.js
+++ code-editor-frontend/src/App.js
@@ -1,29 +1,15 @@
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import LandingPage from "./pages/LandingPage/LandingPage";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  Outlet,
-} from "react-router-dom";
-import { RouterProvider} from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      {
       <Route path="/" element={<LandingPage />} />
-
-      /*
-        <Route path="/" element={<Home />} />
-        <Route path="/Services" element={<ServicesSection />} />
-        <Route path="/services/:serviceId" element={<ServiceDetail />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/contact" element={<ContactUsSection />} />
-        <Route path="/Login" exact element={<Login />} />
-      </Route> 
-      */}
+      <Route path="/home" element={<HomePage />} />
     </>
   )
 );
--- code-editor-frontend/src/pages/HomePage/HomePage.jsx
+++ code-editor-frontend/src/pages/HomePage/HomePage.jsx
@@ -1,1 +1,10 @@
+import Navbar from "../../components/navbar/navbar";
+export default function HomePage(){
+return(
+    <div className="Hompage">
+    <Navbar/>
+</div>
 
+)
+
+}
