c5375866110060b6d56ba9d2727fc974e5ed8357
husseinassidi <husseinassidi@gmail.com>
2024-07-26 20:35:23 +0300

    okok

--- code-editor-frontend/src/App.js
+++ code-editor-frontend/src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import LandingPage from "./pages/LandingPage/LandingPage";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   Outlet,
 } from "react-router-dom";
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      {/*
-      <Route element={<WithNavbarLayout />}>
+      {
+      <Route path="/" element={<LandingPage />} />
+
+      /*
         <Route path="/" element={<Home />} />
         <Route path="/Services" element={<ServicesSection />} />
         <Route path="/services/:serviceId" element={<ServiceDetail />} />
