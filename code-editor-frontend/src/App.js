eaae8d84880d433b1d7e0bfbeebd265d8bd41e0d
karniib <hadikarnib03@gmail.com>
2024-07-26 17:47:15 +0300

    initialized app

--- code-editor-frontend/src/App.js
+++ code-editor-frontend/src/App.js
@@ -1,10 +1,38 @@
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {/*
+      <Route element={<WithNavbarLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/Services" element={<ServicesSection />} />
+        <Route path="/services/:serviceId" element={<ServiceDetail />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUsSection />} />
+        <Route path="/Login" exact element={<Login />} />
+      </Route> 
+      */}
+    </>
+  )
+);
 
 function App() {
-  <ChakraProvider>
-    return <div className="App"></div>;
-  </ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
