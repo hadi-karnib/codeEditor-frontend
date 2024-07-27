abb1d370ee0359b1e87cc22a28fe585b8a694e2c
karniib <hadikarnib03@gmail.com>
2024-07-27 16:37:09 +0300

    added codeEditor.jsx

--- code-editor-frontend/src/components/code-editor/CodeEditor.jsx
+++ code-editor-frontend/src/components/code-editor/CodeEditor.jsx
@@ -1,7 +1,9 @@
-import { Box } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
 import React, { useRef, useState } from "react";
 import { Editor } from "@monaco-editor/react";
 import LanguageSelector from "../../components/code-editor/LanguageSelector";
+import { CODE_SNIPPETS } from "../../constants";
+import Output from "./Output";
 
 const CodeEditor = () => {
   const [value, setValue] = useState("");
@@ -10,21 +12,28 @@ const CodeEditor = () => {
     editorRef.current = editor;
     editor.focus();
   };
-  const [language, setlanguage] = useState("javascript");
+  const [language, setlanguage] = useState("python");
   const onSelect = (language) => {
     setlanguage(language);
+    setValue(CODE_SNIPPETS[language]);
   };
   return (
-    <Box w="50%" h="75%" p="0 60px">
-      <LanguageSelector language={language} onSelect={onSelect} />
-      <Editor
-        defaultLanguage="javascript"
-        language={language}
-        theme="vs-dark"
-        value={value}
-        onChange={(value) => setValue(value)}
-        onMount={onMount}
-      />
+    <Box h={"100%"}>
+      <HStack spacing={4} px={"55px"}>
+        <Box w="50%" h={"100%"}>
+          <LanguageSelector language={language} onSelect={onSelect} />
+          <Editor
+            defaultLanguage={CODE_SNIPPETS[language]}
+            language={language}
+            theme="vs-dark"
+            value={value}
+            onChange={(value) => setValue(value)}
+            onMount={onMount}
+            height="calc(100vh - 50px)"
+          />
+        </Box>
+        <Output editorRef={editorRef} language={language} />
+      </HStack>
     </Box>
   );
 };
