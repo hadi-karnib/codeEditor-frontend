a16e351872bf6692a8444bfd09086bebec3a31b7
karniib <hadikarnib03@gmail.com>
2024-07-31 01:13:57 +0300

    added page protection using jwt

--- code-editor-frontend/src/pages/CodeEditor/CodeEditorPage.jsx
+++ code-editor-frontend/src/pages/CodeEditor/CodeEditorPage.jsx
@@ -1,8 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom"; // Correct import
 import "./CodeEditor.css";
 import Navbar from "../../components/navbar/navbar";
 import CodeEditor from "../../components/code-editor/CodeEditor";
+import { jwtDecode } from "jwt-decode"; // Import jwtDecode
+
 const Editor = () => {
+  const navigate = useNavigate(); // Initialize navigate
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login", { replace: true });
+      return;
+    }
+
+    const decodedToken = jwtDecode(token);
+    if (decodedToken.role !== "user" && decodedToken.role !== "admin") {
+      navigate("/login", { replace: true });
+      return;
+    }
+  }, [navigate]);
+
   return (
     <div className="EditorMainDiv">
       <Navbar />
